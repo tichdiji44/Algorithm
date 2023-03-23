2ba22de3b5c0285e37529ede036b9931f79c5cc9
SHI YU <459123565@qq.com>
2023-03-23 19:27:46 +0800

    单链表百度面试题-从尾到头打印单链表

--- DataStructure/src/com/atguigu/linkedlist/SingleLinkedListDemo.java
+++ DataStructure/src/com/atguigu/linkedlist/SingleLinkedListDemo.java
@@ -1,6 +1,7 @@
 package com.atguigu.linkedlist;
 
 import java.util.Objects;
+import java.util.Stack;
 
 public class SingleLinkedListDemo {
 
@@ -25,9 +26,12 @@ public class SingleLinkedListDemo {
         System.out.println("原来链表的情况~~");
         singleLinkedList.list();
 
-        System.out.println("反转单链表~~");
-        reverseList(singleLinkedList.getHead());
-        singleLinkedList.list();
+//        System.out.println("反转单链表~~");
+//        reverseList(singleLinkedList.getHead());
+//        singleLinkedList.list();
+
+        System.out.println("测试逆序打印单链表，没有改变链表的结构~~");
+        reversePrint(singleLinkedList.getHead());
 
         /*
         // 加入按照编号的顺序
@@ -64,29 +68,38 @@ public class SingleLinkedListDemo {
          */
     }
 
-    // 将单链表反转
+    // 方法2：
+    // 可以利用 栈这个数据结构，将各个节点压入到栈中，然后利用节点先进后出的特点，就实现了逆序打印的效果
     public static void reverseList(HeroNode head) {
         // 如果当前链表为空，或者只有一个节点，无需反转，直接返回
         if (head.next == null || head.next.next == null) {
             return;
         }
-
         // 定义一个辅助的指针（变量），帮助我们遍历原来的链表
         HeroNode cur = head.next;
-        HeroNode next = null; // 指向当前节点[cur]的下一个节点
-        HeroNode reverseHead = new HeroNode(0, "", "");
-        // 遍历原来的链表，每遍历一个节点，就将其取出，并放在新的链表reverseHead的最前端
-        // 动脑筋
-        while (cur != null) {
-            next = cur.next; // 先暂时保存当前节点的下一个节点，因为后面需要使用
-            cur.next = reverseHead.next; // 将cur的下一个节点指向新的链表的最前端
-            reverseHead.next = cur; // 将cur连接到新的链表上
-            cur = next; // 让cur后移
-        }
-        // 将head.next指向reverseHead.next，实现单链表的反转
-        head.next = reverseHead.next;
+        HeroNode next = null;
     }
 
+    // 方式2
+    // 可以利用栈这个数据结构，将各个节点压入到栈中，然后利用栈的先进后出的特点，就实现了逆序打印的效果
+    public static void reversePrint(HeroNode head) {
+        // 如果当前链表为空，或者只有一个节点，无需反转，直接返回
+        if (head.next == null) {
+            return; // 空链表，不能打印
+        }
+        // 创建要给一个栈，将各个节点压入栈
+        Stack<HeroNode> stack = new Stack<>();
+        HeroNode cur = head.next;
+        // 将链表的所有节点压入栈
+        while (cur != null) {
+            stack.push(cur);
+            cur = cur.next; // cur后移，这样就可以压入下一个节点
+        }
+        //将栈中的节点进行打印，pop出栈
+        while (stack.size() > 0) {
+            System.out.println(stack.pop()); // stack的特点是先进后出
+        }
+    }
 
     /**
      * @param head 链表的头结点
