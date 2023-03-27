d8429e55815d4e62ad521ee4b0cb67d7aea82c73
SHI YU <459123565@qq.com>
2023-03-27 19:05:17 +0800

    八皇后问题

---
+++ DataStructure/src/com/atguigu/recursion/Queue8.java
@@ -0,0 +1,71 @@
+package com.atguigu.recursion;
+
+import com.sun.scenario.effect.impl.state.AccessHelper;
+
+public class Queue8 {
+
+    // 定义一个max表示共有多少个皇后
+    int max = 8;
+    // 定义数组array，保存皇后放置位置的结果，比如={0,4,7,5,2,6,1,3}
+    int[] array = new int[max];
+
+    static int count = 0;
+
+    public static void main(String[] args) {
+        // 测试一把，8皇后是否正确
+        Queue8 queue8 = new Queue8();
+        queue8.check(0);
+        System.out.printf("一共有%d解法", count);
+    }
+
+    // 编写一个方法，放置第n个皇后
+    private void check(int n) {
+        if (n == max) { // n = 8，其实8个皇后既然放好
+            print();
+            return;
+        }
+
+        // 依次放入皇后，并判断是否冲突
+        for (int i = 0; i < max; i++) {
+            // 先把当前这个皇后n，放到改行的第1列
+            array[n] = i;
+            // 判断当放置第n个皇后到i列时，是否冲突
+            if (judge(n)) { // 不冲突
+                // 接着放n+1个皇后，并开始递归
+                check(n + 1);
+            }
+
+            // 如果冲突，就继续执行array[n] = i; 即将第n个皇后，放置在本行的后移一个位置
+        }
+    }
+
+    /**
+     * @param n 表示第n个皇后
+     * @return
+     */
+    // 查看当我们放置第n个皇后，就去检测该皇后是否和前面已经摆放的皇后冲突
+    private boolean judge(int n) {
+        for (int i = 0; i < n; i++) {
+            // 说明
+            // 1. array[i] == array[n]  表示判断 第n个皇后是否和前面的n-1个皇后在同一列
+            // 2. Math.abs(n-i) == Math.abs(array[n] - array[i]) 表示判断第n个皇后是否和第i皇后是否在同一斜线
+            // n = 1 放在第2列 n = 1 array[1] = 1
+            // Math.abs(1-0) == 1  Math.abs(array[n] - array[i]) = Math.abs(1-0) = 1
+            // 3. 判断是否在同一行，没有必要，n每次都在递增
+            if (array[i] == array[n] || Math.abs(n - i) == Math.abs(array[n] - array[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 写一个方法，可以将皇后摆放的位置输出
+    private void print() {
+        count++;
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
