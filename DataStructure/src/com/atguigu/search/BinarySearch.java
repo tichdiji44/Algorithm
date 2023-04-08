29266d25dcf0d518ec5db1e9fd5a389427b2ea65
SHI YU <459123565@qq.com>
2023-04-08 10:15:46 +0800

    二分查找

---
+++ DataStructure/src/com/atguigu/search/BinarySearch.java
@@ -0,0 +1,103 @@
+package com.atguigu.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 注意：使用二分查找的前提是 该数组是有序的。
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int arr[] = {1, 8, 10, 89, 1000, 1000, 1234};
+        // int resIndex = binarySearch(arr, 0, arr.length - 1, 88);
+        // System.out.println("resIndex=" + resIndex);
+
+        List resIndexList = binarySearch2(arr, 0, arr.length - 1, 1000);
+        System.out.println("resIndexList=" + resIndexList);
+    }
+
+    /**
+     * 二分查找算法
+     *
+     * @param arr     数组
+     * @param left    左边的索引
+     * @param right   右边的索引
+     * @param findVal 要查找的值
+     * @return 如果找到就返回下标，如果没有找到，就返回-1
+     */
+    public static int binarySearch(int[] arr, int left, int right, int findVal) {
+
+        // 当left > right 时，说明递归整个数组，但是没有找到
+        if (left > right) {
+            return -1;
+        }
+        int mid = (left + right) / 2;
+        int midVal = arr[mid];
+
+        if (findVal > midVal) { // 向右递归
+            return binarySearch(arr, mid + 1, right, findVal);
+        } else if (findVal < midVal) {
+            return binarySearch(arr, left, mid - 1, findVal);
+        } else {
+            return mid;
+        }
+    }
+
+    // 完成一个课后思考题：
+    /*
+    课后思考题：{1, 8, 10, 89, 1000, 1000, 1234} 当一个有序数组中，
+    有多个相同的数值时，如何将所有数值都查找到，比如这里的1000
+
+    思路分析
+    1. 在找到mid索引值，不要马上返回
+    2. 向mid索引值的左边扫描，将所有满足1000的元素的下标，加入到ArrayList
+    3. 向mid索引值的右边扫描，将所有满足1000的元素的下标，加入到ArrayList
+    4. 将ArrayList返回
+     */
+    public static List binarySearch2(int[] arr, int left, int right, int findVal) {
+
+        // 当left > right 时，说明递归整个数组，但是没有找到
+        if (left > right) {
+            return new ArrayList<Integer>();
+        }
+        int mid = (left + right) / 2;
+        int midVal = arr[mid];
+
+        if (findVal > midVal) { // 向右递归
+            return binarySearch2(arr, mid + 1, right, findVal);
+        } else if (findVal < midVal) {
+            return binarySearch2(arr, left, mid - 1, findVal);
+        } else {
+            // 思路分析
+            // 1. 在找到mid索引值，不要马上返回
+            // 2. 向mid索引值的左边扫描，将所有满足1000的元素的下标，加入到ArrayList
+            // 3. 向mid索引值的右边扫描，将所有满足1000的元素的下标，加入到ArrayList
+            // 4. 将ArrayList返回
+            List<Integer> resIndexlist = new ArrayList<>();
+            // 向mid索引值的左边扫描，将所有满足1000的元素的下标，加入到集合ArrayList
+            int temp = mid - 1;
+            while (true) {
+                if (temp < 0 || arr[temp] != findVal) { // 退出
+                    break;
+                }
+                // 否则，就temp放入到resIndexlist
+                resIndexlist.add(temp);
+                temp -= 1; // temp左移
+            }
+            resIndexlist.add(mid);
+
+            // 向mid索引值的右边扫描，将所有满足1000的元素的下标，加入到ArrayList
+            temp = mid + 1;
+            while (true) {
+                if (temp > arr.length - 1 || arr[temp] != findVal) { // 退出
+                    break;
+                }
+                // 否则，就temp 放入到tesIndexlist
+                resIndexlist.add(temp);
+                temp += 1; // temp右移
+            }
+
+            return resIndexlist;
+        }
+    }
+
+}
