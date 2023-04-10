2c8413e07e1e3a86e8dc8ed513e6b1d7c01874f7
SHI YU <459123565@qq.com>
2023-04-10 09:46:49 +0800

    顺序存储二叉树

---
+++ DataStructure/src/com/atguigu/tree/ArrBinaryTreeDemo.java
@@ -0,0 +1,44 @@
+package com.atguigu.tree;
+
+public class ArrBinaryTreeDemo {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        // 创建一个ArrBinaryTree
+        ArrBinaryTree arrBinaryTree = new ArrBinaryTree(arr);
+        arrBinaryTree.preOrder(0); // 1,2,4,5,3,6,7
+    }
+
+}
+
+class ArrBinaryTree {
+
+    private int[] arr; // 存储数据节点的数组
+
+    public ArrBinaryTree(int[] arr) {
+        this.arr = arr;
+    }
+
+    /**
+     * 编写一个方法，完成顺序存储二叉树的前序遍历
+     *
+     * @param index 数组的下标
+     */
+    public void preOrder(int index) {
+        // 如果数组为空，或者arr.length = -1
+        if (arr == null || arr.length == 0) {
+            System.out.println("数组为空，不能按照二叉树的前序遍历");
+        }
+        // 输出当前这个元素
+        System.out.println(arr[index]);
+        // 向左递归遍历
+        if ((index * 2 + 1) < arr.length) {
+            preOrder(2 * index + 1);
+        }
+        // 向右递归遍历
+        if ((index * 2 + 2) < arr.length) {
+            preOrder(2 * index + 2);
+        }
+    }
+
+}
